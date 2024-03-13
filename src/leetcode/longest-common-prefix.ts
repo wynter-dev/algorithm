df66635145acf7c4751fd329f97d63e11497be69
wynter <u0lee.dev@gmail.com>
2024-03-13 17:35:44 +0900

    longest-common-prefix

--- src/leetcode/longest-common-prefix.ts
+++ src/leetcode/longest-common-prefix.ts
@@ -1,14 +1,25 @@
-const longestCommonPrefix = function(strs) {
+const longestCommonPrefix = function(strs: Array<string>) {
     const firstStr = strs[0];
+    let counter = 0;
+    if(!firstStr.length){
+        return '';
+    }
     for(let i = 0; i < firstStr.length; i++){
         const str = firstStr.slice(0,i+1);
-        console.log(str);
+          if(strs.length === 1){
+            return str;
+        }
         const isDiffrent = !strs.every(value => value.slice(0,i+1) === str);
-        
+        counter++;
         if(isDiffrent){
             return str.slice(0,i);
         }
-        
+        if(counter === firstStr.length){
+            return str;
+        }
+
     }
-     
 };
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
+console.log(longestCommonPrefix(["dog","racecar","car"]));
